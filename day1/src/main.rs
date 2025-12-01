be894f3a7b20788602c2144784b31ba71bf950d7
Jim MacArthur <git@mode7.co.uk>
2025-12-01 21:00:47 +0000

    Day 1: Finished Rust solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,17 +2,23 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn parse_token(text: &str) -> i32 {
-   let absval: i32 = match text[1..].parse() {
-     Ok(i) => i,
-     Err(_) => { panic!("Unreadable numeric field {}", text); }
-   };
- 
-   match text.chars().nth(0) {
-     Some('L') => -absval,
-     Some('R') => absval,
-     Some(_) => { panic!("Unreadable field {}", text); },
-     None  => { panic!("No char?"); }
-   }
+    let absval: i32 = match text[1..].parse() {
+        Ok(i) => i,
+        Err(_) => {
+            panic!("Unreadable numeric field {}", text);
+        }
+    };
+
+    match text.chars().next() {
+        Some('L') => -absval,
+        Some('R') => absval,
+        Some(_) => {
+            panic!("Unreadable field {}", text);
+        }
+        None => {
+            panic!("No char?");
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -20,14 +26,36 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut token_iterator = contents.split_whitespace();
+    let token_iterator = contents.split_whitespace();
     let mut list1 = Vec::new();
 
-    while let Some(s) = token_iterator.next() {
-    	  list1.push(parse_token(s));
+    for s in token_iterator {
+        list1.push(parse_token(s));
     }
 
-    println!("{:?}", list1);
+    let mut position: i32 = 50;
+    let mut zero_hits: i32 = 0;
+    for i in list1.iter() {
+        position = (position + i) % 100;
+        if position == 0 {
+            zero_hits += 1;
+        }
+    }
+    println!("Zero passes (part 1): {}", zero_hits);
+    position = 50;
+    zero_hits = 0;
+    for i in list1.iter() {
+        let direction = i.signum();
+        let mut amount = i.abs();
+        while amount > 0 {
+            position = (position + direction) % 100;
+            if position == 0 {
+                zero_hits += 1;
+            }
+            amount -= 1;
+        }
+    }
+    println!("Zero passes (part 2): {}", zero_hits);
     println!("Hello, world!");
     Ok(())
 }
